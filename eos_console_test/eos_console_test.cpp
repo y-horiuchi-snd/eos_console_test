271248e8570863c3cd7aec09bb745ef2b02bd8bc
y_horiuchi <y_horiuchi@sandlot.jp>
2023-11-17 20:56:20 +0900

    初期化、プラットフォームハンドル作成、終了 ハンドルの管理クラス追加 エラー識別用の簡易クラス追加

--- eos_console_test/eos_console_test.cpp
+++ eos_console_test/eos_console_test.cpp
@@ -1,4 +1,114 @@
 ﻿// eos_console_test.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
 //
 
-int main() { return 0; }
+#include "../credentials.h"
+#include <eos_sdk.h>
+#include <malloc.h>
+#include <cassert>
+#include <windows.h>
+#include "eos_error.h"
+#include "eos_handle.h"
+
+#pragma comment(lib, "EOSSDK-Win64-Shipping.lib")
+
+class Memory
+{
+public:
+    static void* AllocateFunc(size_t SizeInBytes, size_t Alignment);
+    static void* ReallocateFunc(void* p, size_t SizeInBytes, size_t Alignment);
+    static void  ReleaseFunc(void* p);
+};
+
+const char* GetTempDirectory();
+
+int main()
+{
+    ::eos::Handle<EOS_HPlatform> platform;
+    {
+        EOS_InitializeOptions options    = {};
+        options.ApiVersion               = EOS_INITIALIZE_API_LATEST;
+        options.AllocateMemoryFunction   = Memory::AllocateFunc;
+        options.ReallocateMemoryFunction = Memory::ReallocateFunc;
+        options.ReleaseMemoryFunction    = Memory::ReleaseFunc;
+        options.ProductName              = SampleConstants::GameName;
+        options.ProductVersion           = "1.0";
+        options.Reserved                 = nullptr;
+        options.SystemInitializeOptions  = nullptr;
+        options.OverrideThreadAffinity   = nullptr;
+
+        const eos::Error r = EOS_Initialize(&options);
+        assert(r.IsSuccess());
+    }
+
+    {
+        EOS_Platform_Options options = {};
+        options.ApiVersion           = EOS_PLATFORM_OPTIONS_API_LATEST;
+        options.bIsServer            = EOS_FALSE;
+        options.EncryptionKey        = SampleConstants::EncryptionKey;
+        options.OverrideCountryCode  = nullptr;
+        options.OverrideLocaleCode   = nullptr;
+        options.Flags                = 0;
+        options.CacheDirectory       = GetTempDirectory();
+
+        options.ProductId    = SampleConstants::ProductId;
+        options.SandboxId    = SampleConstants::SandboxId;
+        options.DeploymentId = SampleConstants::DeploymentId;
+
+        options.ClientCredentials.ClientId     = SampleConstants::ClientCredentialsId;
+        options.ClientCredentials.ClientSecret = SampleConstants::ClientCredentialsSecret;
+
+        platform.Initialize(EOS_Platform_Create(&options), EOS_Platform_Release);
+        assert((bool)platform);
+    }
+
+    platform.Release();
+    {
+        const eos::Error r = EOS_Shutdown();
+        assert(r.IsSuccess());
+    }
+
+    return 0;
+}
+
+void* Memory::AllocateFunc(size_t SizeInBytes, size_t Alignment)
+{
+#if defined(_WIN32)
+    return _aligned_malloc(SizeInBytes, Alignment);
+#else
+    return aligned_alloc(Alignment, SizeInBytes);
+#endif
+}
+void* Memory::ReallocateFunc(void* p, size_t SizeInBytes, size_t Alignment)
+{
+#if defined(_WIN32)
+    return _aligned_realloc(p, SizeInBytes, Alignment);
+#else
+    return reallocalign(p, SizeInBytes, Alignment);
+#endif
+}
+void Memory::ReleaseFunc(void* p)
+{
+#if defined(_WIN32)
+    _aligned_free(p);
+#else
+    free(p);
+#endif
+}
+
+const char* GetTempDirectory()
+{
+#ifdef _WIN32
+    static char Buffer[1024] = {0};
+    if (Buffer[0] == 0)
+    {
+        GetTempPathA(sizeof(Buffer), Buffer);
+    }
+
+    return Buffer;
+
+#elif defined(__APPLE__)
+    return "/private/var/tmp";
+#else
+    return "/var/tmp";
+#endif
+}
---
+++ eos_console_test/eos_handle.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+
+#include <memory>
+
+namespace eos
+{
+
+    /// @brief EOSハンドルを安全に管理するためのオブジェクト
+    template <typename TValue> class Handle
+    {
+    public:
+        /// @brief ハンドル開放用のメソッド
+        using RELEASE_METHOD = void (*)(TValue);
+
+    private:
+        /// @brief ハンドルを実際に管理するためのオブジェクト
+        class Object
+        {
+            TValue         m_handle;
+            RELEASE_METHOD m_release;
+
+        public:
+            Object(TValue handle, RELEASE_METHOD release) : m_handle(handle), m_release(release) {}
+            ~Object() { Release(); }
+
+            void Release()
+            {
+                if (m_handle)
+                {
+                    m_release(m_handle);
+                    m_handle = nullptr;
+                }
+            }
+
+            TValue GetHandle() { return m_handle; }
+        };
+
+        std::shared_ptr<Object> m_object;
+
+    public:
+        Handle() {}
+        Handle(TValue handle, RELEASE_METHOD release) { Initialize(handle, release); }
+
+        /// @brief 初期化するハンドルと解放方法を指定する
+        /// @param handle 登録するハンドル
+        /// @param release 解放処理
+        void Initialize(TValue handle, RELEASE_METHOD release) { m_object = std::make_shared<Object>(handle, release); }
+
+        /// @brief 不要になったので開放する
+        void Release() { m_object.reset(); }
+
+        /// @brief 中身が存在するかどうか
+        operator bool() const noexcept { return (bool)m_object; }
+        bool operator!() const noexcept { return !m_object; }
+
+        /// @brief ハンドルを参照する
+        TValue operator*() const { return m_object->GetHandle(); }
+        TValue operator->() const { return m_object->GetHandle(); }
+        operator TValue() const { return m_object->GetHandle(); }
+    };
+
+} // namespace eos
